c9385be56c988c908489e8cda7c2e583c64c73c2
matthewlesko01 <mlesko1996@gmail.com>
2017-11-24 15:18:58 -0500

    Updated rules for j,q,k = 10

--- Lab4/g27_rules.vhd
+++ Lab4/g27_rules.vhd
@@ -30,6 +30,7 @@ ARCHITECTURE architecture_rules OF g27_rules IS
 	 
 	 -- UNSIGNED signals
     SIGNAL face_value : UNSIGNED(3 DOWNTO 0);
+	 SIGNAL face_value_buffer : UNSIGNED(3 DOWNTO 0);
     SIGNAL current_sum : UNSIGNED(4 DOWNTO 0);
     SIGNAL new_sum : UNSIGNED(4 DOWNTO 0);
 
@@ -72,11 +73,19 @@ ARCHITECTURE architecture_rules OF g27_rules IS
 	-- All additions and legal_play checking are done under here
 	ADDITIONS_AND_COMPARISONS: PROCESS (face_value, new_sum, current_sum, legal_play_signal)
 	BEGIN
-		-- If we bust but we have an ace in our hand, decrease hand by 10
-		IF (play_pile_top_card(5) = '1' and ((current_sum + face_value) > 21)) then
-			new_sum <= current_sum + face_value - 10;
+		
+		-- If face_value >= 11, then make it 10
+		IF (face_value >= 11) then
+			face_value_buffer <= to_unsigned(10, 4);
 		ELSE
-			new_sum <= current_sum + face_value;
+			face_value_buffer <= face_value;
+		END IF;
+		
+		-- If we bust but we have an ace in our hand, decrease hand by 10
+		IF (play_pile_top_card(5) = '1' and ((current_sum + face_value_buffer) > 21)) then
+			new_sum <= current_sum + face_value_buffer - 10;
+		ELSE
+			new_sum <= current_sum + face_value_buffer;
 		END IF;
 		
 		IF (new_sum <= 21) then
