1d7780e6d61c831d719818d6678a3ea7712b12e6
romnith <romainnith@hotmail.com>
2017-11-23 21:46:19 -0500

    something is wrong here.... all values are 0

--- Lab3/lpm_mux1.vhd
+++ Lab3/lpm_mux1.vhd
@@ -42,10 +42,10 @@ USE lpm.lpm_components.all;
 ENTITY lpm_mux1 IS
 	PORT
 	(
-		data0		: IN STD_LOGIC ;
-		data1		: IN STD_LOGIC ;
+		data0x		: IN STD_LOGIC_VECTOR (5 DOWNTO 0);
+		data1x		: IN STD_LOGIC_VECTOR (5 DOWNTO 0);
 		sel		: IN STD_LOGIC ;
-		result		: OUT STD_LOGIC 
+		result		: OUT STD_LOGIC_VECTOR (5 DOWNTO 0)
 	);
 END lpm_mux1;
 
@@ -54,34 +54,42 @@ ARCHITECTURE SYN OF lpm_mux1 IS
 
 --	type STD_LOGIC_2D is array (NATURAL RANGE <>, NATURAL RANGE <>) of STD_LOGIC;
 
-	SIGNAL sub_wire0	: STD_LOGIC_VECTOR (0 DOWNTO 0);
-	SIGNAL sub_wire1	: STD_LOGIC ;
-	SIGNAL sub_wire2	: STD_LOGIC ;
-	SIGNAL sub_wire3	: STD_LOGIC_2D (1 DOWNTO 0, 0 DOWNTO 0);
+	SIGNAL sub_wire0	: STD_LOGIC_VECTOR (5 DOWNTO 0);
+	SIGNAL sub_wire1	: STD_LOGIC_VECTOR (5 DOWNTO 0);
+	SIGNAL sub_wire2	: STD_LOGIC_2D (1 DOWNTO 0, 5 DOWNTO 0);
+	SIGNAL sub_wire3	: STD_LOGIC_VECTOR (5 DOWNTO 0);
 	SIGNAL sub_wire4	: STD_LOGIC ;
-	SIGNAL sub_wire5	: STD_LOGIC ;
-	SIGNAL sub_wire6	: STD_LOGIC_VECTOR (0 DOWNTO 0);
+	SIGNAL sub_wire5	: STD_LOGIC_VECTOR (0 DOWNTO 0);
 
 BEGIN
-	sub_wire4    <= data0;
-	sub_wire1    <= sub_wire0(0);
-	result    <= sub_wire1;
-	sub_wire2    <= data1;
-	sub_wire3(1, 0)    <= sub_wire2;
-	sub_wire3(0, 0)    <= sub_wire4;
-	sub_wire5    <= sel;
-	sub_wire6(0)    <= sub_wire5;
+	sub_wire3    <= data0x(5 DOWNTO 0);
+	result    <= sub_wire0(5 DOWNTO 0);
+	sub_wire1    <= data1x(5 DOWNTO 0);
+	sub_wire2(1, 0)    <= sub_wire1(0);
+	sub_wire2(1, 1)    <= sub_wire1(1);
+	sub_wire2(1, 2)    <= sub_wire1(2);
+	sub_wire2(1, 3)    <= sub_wire1(3);
+	sub_wire2(1, 4)    <= sub_wire1(4);
+	sub_wire2(1, 5)    <= sub_wire1(5);
+	sub_wire2(0, 0)    <= sub_wire3(0);
+	sub_wire2(0, 1)    <= sub_wire3(1);
+	sub_wire2(0, 2)    <= sub_wire3(2);
+	sub_wire2(0, 3)    <= sub_wire3(3);
+	sub_wire2(0, 4)    <= sub_wire3(4);
+	sub_wire2(0, 5)    <= sub_wire3(5);
+	sub_wire4    <= sel;
+	sub_wire5(0)    <= sub_wire4;
 
 	LPM_MUX_component : LPM_MUX
 	GENERIC MAP (
 		lpm_size => 2,
 		lpm_type => "LPM_MUX",
-		lpm_width => 1,
+		lpm_width => 6,
 		lpm_widths => 1
 	)
 	PORT MAP (
-		data => sub_wire3,
-		sel => sub_wire6,
+		data => sub_wire2,
+		sel => sub_wire5,
 		result => sub_wire0
 	);
 
@@ -98,16 +106,16 @@ END SYN;
 -- Retrieval info: LIBRARY: lpm lpm.lpm_components.all
 -- Retrieval info: CONSTANT: LPM_SIZE NUMERIC "2"
 -- Retrieval info: CONSTANT: LPM_TYPE STRING "LPM_MUX"
--- Retrieval info: CONSTANT: LPM_WIDTH NUMERIC "1"
+-- Retrieval info: CONSTANT: LPM_WIDTH NUMERIC "6"
 -- Retrieval info: CONSTANT: LPM_WIDTHS NUMERIC "1"
--- Retrieval info: USED_PORT: data0 0 0 0 0 INPUT NODEFVAL "data0"
--- Retrieval info: USED_PORT: data1 0 0 0 0 INPUT NODEFVAL "data1"
--- Retrieval info: USED_PORT: result 0 0 0 0 OUTPUT NODEFVAL "result"
+-- Retrieval info: USED_PORT: data0x 0 0 6 0 INPUT NODEFVAL "data0x[5..0]"
+-- Retrieval info: USED_PORT: data1x 0 0 6 0 INPUT NODEFVAL "data1x[5..0]"
+-- Retrieval info: USED_PORT: result 0 0 6 0 OUTPUT NODEFVAL "result[5..0]"
 -- Retrieval info: USED_PORT: sel 0 0 0 0 INPUT NODEFVAL "sel"
--- Retrieval info: CONNECT: @data 1 0 1 0 data0 0 0 0 0
--- Retrieval info: CONNECT: @data 1 1 1 0 data1 0 0 0 0
+-- Retrieval info: CONNECT: @data 1 0 6 0 data0x 0 0 6 0
+-- Retrieval info: CONNECT: @data 1 1 6 0 data1x 0 0 6 0
 -- Retrieval info: CONNECT: @sel 0 0 1 0 sel 0 0 0 0
--- Retrieval info: CONNECT: result 0 0 0 0 @result 0 0 1 0
+-- Retrieval info: CONNECT: result 0 0 6 0 @result 0 0 6 0
 -- Retrieval info: GEN_FILE: TYPE_NORMAL lpm_mux1.vhd TRUE
 -- Retrieval info: GEN_FILE: TYPE_NORMAL lpm_mux1.inc FALSE
 -- Retrieval info: GEN_FILE: TYPE_NORMAL lpm_mux1.cmp TRUE
