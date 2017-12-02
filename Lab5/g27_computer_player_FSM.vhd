3a6e829588ae3f1584ebacd1e1e8a202fedc0860
matthewlesko01 <mlesko1996@gmail.com>
2017-12-01 20:56:40 -0500

    Updated computer player fsm

--- Lab5/g27_computer_player_FSM.vhd
+++ Lab5/g27_computer_player_FSM.vhd
@@ -7,6 +7,7 @@
 -- Author: Matthew Lesko-Krleza 260692352, matthew.lesko-krleza@mail.mcgill.ca 
 --         Romain Nith 260571471, romain.nith@mail.mcgill.ca
 -- Date: 30/11/17
+-- Updated: 01/12/17 (Matthew), 
 
 library ieee; -- allows use of the std_logic_vectory type
 use ieee.std_logic_1164.all;
@@ -16,21 +17,20 @@ use ieee.numeric_std.all;
 ENTITY g27_computer_player_FSM IS
     PORT
     (
-        new_card_value              : in  std_logic_vector (3 downto 0); -- card value will always be 10 or under 10 <= 2^4 = 16
         current_sum                 : in  std_logic_vector (4 downto 0); -- sum will always be 32 or under 32 <= 2^5 = 32
-        turn	                    : in  std_logic;
+        turn	                     : in  std_logic;
         new_hand                    : in  std_logic;
         clk                         : in  std_logic;
         reset                       : in  std_logic;
         request_deal                : out std_logic;
         computer_sum                : out std_logic_vector (4 downto 0); -- sum will always be 32 or under 32 <= 2^5 = 32;
-        turn_finish                 : out std_logic_vector (1 downto 0)
+        turn_finish                 : out std_logic
     );
     END g27_computer_player_FSM;
 
 ARCHITECTURE architecture_computer_player_FSM OF g27_computer_player_FSM IS 
 TYPE state_type is (s0, s1, s2);    -- State declaration
-    SIGNAL current_sum_temp         : std_logic_vector (4 downto 0);
+    SIGNAL current_sum_buffer       : unsigned (4 downto 0);
     SIGNAL state                    : state_type;          -- Init signal that uses the different states
     
 BEGIN 
@@ -40,7 +40,7 @@ BEGIN
             IF (reset = '1') THEN   -- Triggered when reset is high
                 state <= s0;
 
-            ELSIF rising_edge(clk) THEN -- Triggered at rising edge of clock
+            ELSIF rising_edge(clk) THEN -- Triggered at rising edge of clock and it is the computer's turn
 
                 CASE state IS 
                     
@@ -54,12 +54,12 @@ BEGIN
 
                     WHEN s1 =>        -- Sends a request_deal signal
                         -- Add new card value to current computer sum
-                        current_sum_temp <= ("0" & new_card_value) + current_sum;
+                        current_sum_buffer <= unsigned(current_sum);
 
-                        IF (current_sum_temp <= 16) THEN
+                        IF (current_sum_buffer <= 16) THEN
                             state       <= s1; -- continue drawing a card
                         ELSE
-                            state       <= s2; -- stop drawing cards
+                            state       <= s2; -- stop drawing cards and advance to s2
                         END IF;
                 
                     WHEN s2 =>        -- remains in this state if new_hand isn't high
@@ -92,9 +92,9 @@ BEGIN
                 WHEN s1 =>                      -- Request deal
                     request_deal                    <= '1';
                     turn_finish                     <= '0';
-                    computer_sum                    <= current_sum_temp; -- outputs new computer sum
+                    computer_sum                    <= std_logic_vector(current_sum_buffer); -- outputs new computer sum
 
-                WHEN s2 =>                      -- Computer loses                      
+                WHEN s2 =>                      -- Output computer is done                     
                     request_deal                    <= '0';
                     turn_finish                     <= '1';
 
